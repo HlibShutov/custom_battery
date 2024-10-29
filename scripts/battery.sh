96ccc965efe3af48bb5a4221bf9492ec6762df88
HlibShutov <145573612+HlibShutov@users.noreply.github.com>
2024-10-29 14:02:05 +0100

    fixing errors

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,8 +1,15 @@
+notify_file="/tmp/battery_notify_status"
+
+if [ ! -f "$notify_file" ]; then
+    echo "false" > "$notify_file"
+fi
+
 battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 power_profile=$(powerprofilesctl get)
 energy_now=$(cat /sys/class/power_supply/BAT0/energy_now)
 power_now=$(cat /sys/class/power_supply/BAT0/power_now)
 status=$(cat /sys/class/power_supply/BAT0/status)
+notification_sent=$(cat "$notify_file")
 classes=()
 
 if [[ $power_profile = "power-saver" ]]; then
@@ -15,9 +22,17 @@ if [[ $power_profile = "performance" ]]; then
 	classes[0]="performance"
 fi
 
-if [[ $battery_capacity < 15 ]]; then
+if [[ $battery_capacity -lt 15 && "$status" != "Charging" ]]; 
+then
 	classes+=("critical")
+	if [[ $notification_sent == "false" ]]; then
+	    notify-send "Battery is discharging" -u critical
+	    echo "true" > "$notify_file"
+	fi
+else
+        echo "false" > "$notify_file"
 fi
+
 
 text="$battery_capacity%"
 
@@ -44,13 +59,18 @@ else
 	json_classes=\"$classes\"
 fi
 
-if [[ $status != "Charging" ]]; 
+if [[ $power_now > 0 ]]; 
 then
         time_decimal=$(echo "scale=10; $energy_now / $power_now" | bc)
-        hours=${time_decimal%.*}
-        minutes=$(echo "(${time_decimal} - $hours) * 60" | bc)
+	if [[ "$time_decimal" =~ ^\.[0-9]+$ ]]; then
+            time_decimal="0$time_decimal"
+        fi
+	hours=${time_decimal%.*}
+
+        substract_time=$(echo "$time_decimal - $hours" | bc)
+
+        minutes=$(echo "$substract_time * 60" | bc)
         time_to_empty=$(printf "%d hours and %.0f minutes\n" "$hours" "$minutes")
-        echo $time_to_empty
 else
 	time_to_empty="Charging"
 fi
@@ -62,4 +82,3 @@ JSON_FMT="{
 	\"tooltip\": \"$time_to_empty\"
 }"
 echo $JSON_FMT
-
